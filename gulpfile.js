c1b9b85154337a1bc15d40ee0ad954ee4d71385a
tomihei <lolvgmxd@gmail.com>
2018-04-20 16:04:06 +0900

    ALVEサイトテーマ

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+/**
+ * Created by tomino on 18/04/19.
+ */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var jade = require('gulp-jade');
+var browserSync =require('browser-sync');
+
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"       //対象ディレクトリ
+            ,index  : "index.html"      //インデックスファイル
+        }
+    });
+});
+
+
+gulp.task('convertToCss', function() {
+    gulp.src('./src/scss/style.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('jadeindex', () => {
+    return gulp.src('./src/jade/index.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('jade', () => {
+    return gulp.src('./src/jade/**/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./html'));
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['browser-sync'], function() {
+    gulp.watch('./src/scss/*.scss',['convertToCss']);
+    gulp.watch('./src/jade/**/*.jade',['jadeindex','jade']);
+    gulp.watch("./index.html",        ['bs-reload']);
+    gulp.watch("./style.css", ['bs-reload']);
+
+});
