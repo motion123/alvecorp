aad5fda573b6f2fb111bcf5d94ad6c0ef8cd33e3
tomihei <lolvgmxd@gmail.com>
2018-04-24 17:17:44 +0900

    aboutus

--- src/js/scrollevent.js
+++ src/js/scrollevent.js
@@ -6,33 +6,57 @@ var topElement = document.getElementById("top");
 var serviceElement = document.getElementById("service");
 var missionElement = document.getElementById("mission");
 var newsElement = document.getElementById("news");
+var serviceblockElement = document.getElementById("sfc");
+var abouttitleElement = document.getElementById("about");
+var aboutcolElement = document.getElementById("ac");
 
 var topWatcher = scrollMonitor.create( topElement, -100 );
 var serviceWatcher = scrollMonitor.create( serviceElement, -100 );
 var missionWatcher = scrollMonitor.create( missionElement, -100 );
 var newsWatcher = scrollMonitor.create( newsElement, -100 );
+var sbWatcher = scrollMonitor.create( serviceblockElement, -200);
+var abouttitleWatcher = scrollMonitor.create(abouttitleElement,-100);
+var aboutcolWatcher = scrollMonitor.create(aboutcolElement, -150);
+
 
 topWatcher.enterViewport(function() {
     console.log( '表示領域に入りました' );
 });
 missionWatcher.enterViewport(function() {
-    console.log( '表示領域に入りました' );
     var mt = document.getElementById("mt");
     mt.classList.remove("init-title");
     mt.classList.add("title-animation");
     missionWatcher.destroy();
 });
 serviceWatcher.enterViewport(function() {
-    console.log( '表示領域に入りました' );
     var st = document.getElementById("st");
     st.classList.remove("init-title");
     st.classList.add("title-animation");
     serviceWatcher.destroy();
 });
 newsWatcher.enterViewport(function() {
-    console.log( '表示領域に入りました' );
     var nt = document.getElementById("nt");
     nt.classList.remove("init-title");
     nt.classList.add("title-animation");
     newsWatcher.destroy();
+});
+
+sbWatcher.enterViewport(function () {
+    console.log("はいったよ");
+    var sfc = document.getElementById("sfc");
+    sfc.classList.add("sa");
+});
+
+abouttitleWatcher.enterViewport(function () {
+    var at = document.getElementById("at");
+    at.classList.remove("init-title");
+    at.classList.add("title-animation");
+    abouttitleWatcher.destroy();
+});
+
+aboutcolWatcher.enterViewport(function () {
+    var ac = document.getElementById("ac");
+    ac.classList.remove("init-col");
+    ac.classList.add("animate-about-col");
+    aboutcolWatcher.destroy();
 });
